2c4d3adecf0ccd901080128b34e04218cff9e69c
Armin <armin536@gmail.com>
2022-06-02 19:17:47 +0430

    Initial version

--- Package.swift
+++ Package.swift
@@ -5,24 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "WhatsAppStickersThirdParty",
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "WhatsAppStickersThirdParty",
             targets: ["WhatsAppStickersThirdParty"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImageWebPCoder.git", from: "0.8.4")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WhatsAppStickersThirdParty",
-            dependencies: []),
-        .testTarget(
-            name: "WhatsAppStickersThirdPartyTests",
-            dependencies: ["WhatsAppStickersThirdParty"]),
+            dependencies: ["SDWebImageWebPCoder"]
+        ),
     ]
 )
